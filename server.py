c12a965625bfd1e09fff8c06136847f182990b22
Zhening <zhening.yang@gmail.com>
2018-05-17 16:50:41 +0200

    fix

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import json
-import stalker
+from stalker import *
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
@@ -16,10 +16,20 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def run():
     data = json.loads(request.get_data())
     print('start')
-    stalker.function(data)
+    FM = stalker_evolution(stalker_graph(data))
+    print('continue...')
+    stalker_plot(FM, "eigenvector")
+    stalker_plot(FM, "closeness")
+    stalker_plot(FM, "betweenness")
     print('finish')
     return jsonify({'result': len(data)})
 
 
+@app.route('/test', methods=['GET'])
+@cross_origin()
+def test():
+    return jsonify({'State': 'OK'})
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
--- stalker.py
+++ stalker.py
@@ -1,59 +1,35 @@
 #!/usr/bin/env python
 
 import plotly.plotly as py
-import plotly.graph_objs as go
+from plotly.graph_objs import *
 import plotly.tools as pt
 import networkx as nx
-import json
 from networkx.readwrite import json_graph
+import operator
 
 
-def function(data):
-    mapbox_access_token = 'pk.eyJ1IjoiY2xlaXR1cyIsImEiOiJjamgwZ2c1a3Yxc3dtMnFtb2ptdDR5ZWs0In0.sjZdn45v32AojmWGWIN9Tg'
-    pt.set_credentials_file(username='cleitus', api_key='LN8W33LMo7kMNz2LU7Ce')
-
-    # ########################### Reading Initial Data ###################################
-
+def stalker_graph(data):
     nodes = data['nodes']
     links = data['edges']
-
-    # ########################### Reading Initial Data ###################################
-
     for i in links:
         i['value'] = 'init'
-
     M = nx.Graph()
-
     M = nx.Graph(
         [(i['source'], i['target'], {'value': i['value']}) for i in links])
-    for i in range(len(M.nodes)):
-        node = nodes[i]['id']
-        M.add_node(node, group=nodes[i]['group'])
-        M.add_node(node, name=nodes[i]['name'])
-        M.add_node(node, istrain=nodes[i]['istrain'])
-        M.add_node(node, lat=nodes[i]['lat'])
-        M.add_node(node, lon=nodes[i]['lon'])
-        M.add_node(node, id=nodes[i]['id'])
+    for i in nodes:
+        node = i['id']
+        M.add_node(node, salle=i['salle'])
+        M.add_node(node, group=i['group'])
+        M.add_node(node, name=i['name'])
+        M.add_node(node, istrain=i['istrain'])
+        M.add_node(node, age=i['age'])
+        M.add_node(node, lat=i['lat'])
+        M.add_node(node, lon=i['lon'])
+        M.add_node(node, id=i['id'])
+    return M
 
-    # Eigenvector centrality criteria
-    Meigen = nx.eigenvector_centrality(M)
-    normeigen = [float(i) / max(Meigen.values()) for i in Meigen.values()]
 
-    # Closeness centrality
-    Mclose = nx.closeness_centrality(M)
-    normclose = Mclose.values()
-
-    # Betweeness centrality
-    Mbetween = nx.betweenness_centrality(M)
-    normbetween = Mbetween.values()
-
-    N = len(M.nodes())
-    labels = [i[1]['name'] for i in M.nodes(data=True)]
-
-    # ###################### Evolution ####################
-
-    import operator
-
+def stalker_evolution(M):
     # Common Neighbors
     CN = [(e[0], e[1], len(list(nx.common_neighbors(M, e[0], e[1]))))
           for e in nx.non_edges(M)]
@@ -83,7 +59,7 @@ def function(data):
     # CRA = list(nx.ra_index_soundarajan_hopcroft(M))
     # CRA.sort(key=operator.itemgetter(2), reverse = True)
 
-    # ###################### Prediction on Future Edge Linkage ####################
+    # ###################### Prediction of future edge formation ####################
 
     FM = M
     for i in PA[0:int(0.1 * len(M.edges()))]:
@@ -92,25 +68,69 @@ def function(data):
     for i in CN[0:int(0.1 * len(M.edges()))]:
         FM.add_edge(i[0], i[1], value='new')
 
-    # Write out evolved graph data in JSON file
-    jsonData = json_graph.node_link_data(FM)
-    with open('fb_evo.json', 'w') as outfile:
-        json.dump(jsonData, outfile, indent=4)
+    return FM
 
-    # ###################### Plot ####################
+
+def stalker_layout(FM):
+    pos = nx.fruchterman_reingold_layout(FM, dim=3)
+    lay = list()
+    for i in pos.values():
+        lay.append(list(i))
+    return pos, lay
+
+
+def stalker_centrality(FM, mesure):
+    pos = stalker_layout(FM)[0]
+
+    ulti = {}
+    for i in pos.keys():
+        ulti[i] = list(pos[i])
+
+    # Eigenvector centrality criteria (normalised)
+    Geigen = nx.eigenvector_centrality(FM)
+    for i in Geigen:
+        ulti[i].append(float(Geigen[i]) / max(Geigen.values()))
+
+    # Closeness centrality
+    Gclose = nx.closeness_centrality(FM)
+    for i in Gclose:
+        ulti[i].append(Gclose[i])
+
+    # Betweeness centrality
+    Gbetween = nx.betweenness_centrality(FM)
+    for i in Gbetween:
+        ulti[i].append(Gbetween[i])
+
+    if mesure == "eigenvector":
+        return [i[-3] for i in ulti.values()]
+    elif mesure == "closeness":
+        return [i[-2] for i in ulti.values()]
+    elif mesure == "betweenness":
+        return [i[-1] for i in ulti.values()]
+
+
+def stalker_plot(FM, mesure):
+    mapbox_access_token = 'pk.eyJ1IjoiY2xlaXR1cyIsImEiOiJjamgwZ2c1a3Yxc3dtMnFtb2ptdDR5ZWs0In0.sjZdn45v32AojmWGWIN9Tg'
+    # pt.set_credentials_file(username='cleitus', api_key='LN8W33LMo7kMNz2LU7Ce')
+    pt.set_credentials_file(username='zhening', api_key='9LICBZ681YiPTiSZCuFX')
+
+    N = len(FM.nodes())
+    labels = [i[1]['name'] for i in FM.nodes(data=True)]
+
+    # ############################# 2d Map Plot #####################################
 
     # Nodes and Edges coordinates
-    Xv = [k[1]['lat'] for k in M.nodes(data=True)]
-    Yv = [k[1]['lon'] for k in M.nodes(data=True)]
+    Xv = [k[1]['lat'] for k in FM.nodes(data=True)]
+    Yv = [k[1]['lon'] for k in FM.nodes(data=True)]
     Xed = []
     Yed = []
     Xned = []
     Yned = []
-    for edge in M.edges():
-        Xed += [M.nodes(data=True)[edge[0]]['lat'],
-                M.nodes(data=True)[edge[1]]['lat'], None]
-        Yed += [M.nodes(data=True)[edge[0]]['lon'],
-                M.nodes(data=True)[edge[1]]['lon'], None]
+    for edge in FM.edges():
+        Xed += [FM.nodes(data=True)[edge[0]]['lat'],
+                FM.nodes(data=True)[edge[1]]['lat'], None]
+        Yed += [FM.nodes(data=True)[edge[0]]['lon'],
+                FM.nodes(data=True)[edge[1]]['lon'], None]
 
     for edge in [(i[0], i[1]) for i in list(FM.edges(data=True)) if i[2]['value'] == 'new']:
         Xned += [FM.nodes(data=True)[edge[0]]['lat'],
@@ -118,61 +138,108 @@ def function(data):
         Yned += [FM.nodes(data=True)[edge[0]]['lon'],
                  FM.nodes(data=True)[edge[1]]['lon'], None]
 
-    Meigen = nx.eigenvector_centrality(FM)
-    normeigen = [float(i) / max(Meigen.values()) for i in Meigen.values()]
-
     data = [
-        go.Scattermapbox(
+        Scattermapbox(
             lat=Xed,
             lon=Yed,
             mode='lines',
             line=dict(color='rgb(125,125,125)', width=1),
             hoverinfo='none'
         ),
-        go.Scattermapbox(
+        Scattermapbox(
             lat=Xned,
             lon=Yned,
             mode='lines',
             line=dict(color='rgb(158,18,130)', width=1),
             hoverinfo='none'
         ),
-        go.Scattermapbox(
+        Scattermapbox(
             lat=Xv,
             lon=Yv,
             mode='markers',
-            name='actors',
+            name=mesure,
             marker=dict(size=10,
-                        color=normeigen,
-                        # color=normbetween, #
-                        # color=normclose, #
-                        # color=[data['nodes'][k]['group'] for k in range(len(data['nodes']))], #
-                        # colorbar=dict(title=''),
-                        colorscale='Viridis',
-                        # line=dict(color='rgb(158,18,130)', width=0.5)
+
+                        color=stalker_centrality(FM, mesure),
+
+                        colorscale='Viridis'
                         ),
             text=labels,
             hoverinfo='text'
         )
     ]
 
-    layout = go.Layout(
+    layout = Layout(
         autosize=True,
         hovermode='closest',
         mapbox=dict(
             accesstoken=mapbox_access_token,
             bearing=0,
             center=dict(
-                lat=48.856813,
-                lon=2.346654
+                lat=sum(Xv) / N,
+                lon=sum(Yv) / N
             ),
             pitch=0,
-            zoom=18.50
+            zoom=13
         ),
     )
 
     fig = dict(data=data, layout=layout)
-    # py.plot(fig, filename='fb')
-    plot_url = py.plot(fig, filename='fb', auto_open=False)
+    plot_url = py.plot(fig, filename=mesure + '_fb', auto_open=False)
 
-    print('I"m in stalker mode')
-    return plot_url
+    # ############################# 3d Layout Plot #####################################
+
+    pos, lay = stalker_layout(FM)
+
+    # Nodes and Edges coordinates
+    Xv = [lay[k][0] for k in range(N)]  # x-coordinates of nodes
+    Yv = [lay[k][1] for k in range(N)]  # y-coordinates
+    Zv = [lay[k][2] for k in range(N)]  # z-coordinates
+    Xed = []
+    Yed = []
+    Zed = []
+    Xned = []
+    Yned = []
+    Zned = []
+    for edge in FM.edges():
+        Xed += [pos[edge[0]][0], pos[edge[1]][0], None]
+        Yed += [pos[edge[0]][1], pos[edge[1]][1], None]
+        Zed += [pos[edge[0]][2], pos[edge[1]][2], None]
+
+    for edge in [(i[0], i[1]) for i in list(FM.edges(data=True)) if i[2]['value'] == 'new']:
+        Xned += [pos[edge[0]][0], pos[edge[1]][0], None]
+        Yned += [pos[edge[0]][1], pos[edge[1]][1], None]
+        Zned += [pos[edge[0]][2], pos[edge[1]][2], None]
+
+    trace1 = Scatter3d(x=Xed,
+                       y=Yed,
+                       z=Zed,
+                       mode='lines',
+                       line=Line(color='rgb(125,125,125)', width=1),
+                       hoverinfo='none'
+                       )
+
+    trace2 = Scatter3d(x=Xv,
+                       y=Yv,
+                       z=Zv,
+                       mode='markers',
+                       name=mesure,
+                       marker=Marker(symbol='dot',
+
+                                     color=stalker_centrality(FM, mesure),
+
+                                     size=6, colorbar=ColorBar(
+                               title=''
+                           ),
+                                     colorscale='Viridis',
+                                     line=Line(color='rgb(158,18,130)', width=0.5)
+                                     ),
+                       text=labels,  # node Labels
+                       hoverinfo='text'
+                       )
+
+    data = Data([trace1, trace2])
+    plot_url = py.plot(data, filename=mesure + '_fb_3d', auto_open=False)
+
+    print('Stalker mode')
+    return
